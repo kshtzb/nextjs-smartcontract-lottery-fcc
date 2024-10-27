0ed352dd48113ea53d2cfd31adf6a8051e3e0d53
kshtzb <94812815+kshtzb@users.noreply.github.com>
2024-10-27 12:43:31 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: { loader: "custom" },
+  output: 'export' // Add this line for static exports
 }
 
-module.exports = { nextConfig, images: {loader: "custom"}}
+module.exports = nextConfig;
